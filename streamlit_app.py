8c01714711ccf76129d6f2810ac87ef0a4abf8f7
RabbitJackTrade <39741388+RabbitJackTrade@users.noreply.github.com>
2021-09-06 19:37:32 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,14 +11,6 @@ df['Own_pct'] = df.Units_num/ df.Units_num.sum()
 df.loc['Units_total']= df.sum(numeric_only=True, axis=0)
 df['Units_num'] = df['Units_num'].astype(int)
 df.index = ['Member_1', 'Member_2', 'Exec',' Total']
-
-"""
-mushi = [10,20,30,40]
-option = st.sidebar.selectbox(
-    'How many units in the grant?',    
-     mushi)
-'You selected: ', option
-"""
 score = st.sidebar.slider('Select number of units to grant', min_value=5, max_value=40, value = 5) # Getting the input.
 df.at['Exec','Units_num']=score
 df.loc[' Total']= df.iloc[0:3].sum(numeric_only=True, axis=0)
