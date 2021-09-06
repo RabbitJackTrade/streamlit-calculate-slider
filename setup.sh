25955b3c58c121f3af3e8fb24a2e347012fd59cd
RabbitJackTrade <39741388+RabbitJackTrade@users.noreply.github.com>
2021-09-06 19:53:12 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,10 @@
-
+mkdir -p ~/.streamlit/echo "\
+[general]\n\
+email = \"<youremail>\"\n\
+" > ~/.streamlit/credentials.tomlecho "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+" > ~/.streamlit/config.toml
